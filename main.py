fd0e86b3369a57a9a03ba0d927337f64062d6851
Ok Namgung <rareok24@gmail.com>
2026-03-09 13:33:53 +0900

    소스 초기 저장

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World"}
+
+
+@app.get("/employees")
+def read_employee(name: str):
+    return {"message": f"{name}님 반갑습니다.!"}
